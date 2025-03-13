1d1b09f95659b6707e680925acee3cd5f150c905
artemkolodko <temakolodko@gmail.com>
2025-03-13 14:19:15 +0000

    add beefy script

---
+++ src/beefy/index.ts
@@ -0,0 +1,43 @@
+const { data: { clmHarvestEvents } } = require('./harvests.json')
+import Decimal from 'decimal.js'
+
+// const subgraphAPI = 'https://api.goldsky.com/api/public/project_clu2walwem1qm01w40v3yhw1f/subgraphs/beefy-clm-sonic/latest/gn'
+// const subgraphQuery = `
+//   {
+//   clmHarvestEvents(
+//     first:1000
+//     where:{
+//       clm_: {
+//         id:"0xc46833f6217db6586fd129cc2f61361dfce4c21d"
+//       }
+//     }
+//     orderBy: createdWith__blockNumber
+//     orderDirection:desc
+//   ) {
+//     rewardToNativePrices
+//     nativeToUSDPrice
+//   }
+// }
+// `
+
+interface HarvestEvent {
+  rewardToNativePrices: string[]
+  nativeToUSDPrice: string
+}
+
+const main = async () => {
+  let totalAmountUSD = new Decimal(0);
+  (clmHarvestEvents as HarvestEvent[]).forEach(item => {
+    if (item.rewardToNativePrices.length === 0) return
+
+    const nativePrice = new Decimal(item.nativeToUSDPrice).div(10 ** 18)
+    const rewardUSD = new Decimal(item.rewardToNativePrices[0])
+      .div(10**18)
+      .mul(nativePrice)
+    totalAmountUSD = totalAmountUSD.add(rewardUSD)
+  })
+  console.log('totalAmountUSD', totalAmountUSD.toString())
+  console.log('events count:', clmHarvestEvents.length)
+}
+
+main()

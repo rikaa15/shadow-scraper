a87d580682a6d39b3c949fd6ce60e37f59f927b0
artemkolodko <temakolodko@gmail.com>
2025-03-18 12:57:45 +0000

    add userAddress param

--- src/portfolio-tracker/index.ts
+++ src/portfolio-tracker/index.ts
@@ -1,6 +1,6 @@
 import {getInfo} from "./swapx";
 
-const userAddress = '0x4E430992Db6F3BdDbC6A50d1513845f087E9af4A'
+// const userAddress = '0x4E430992Db6F3BdDbC6A50d1513845f087E9af4A'
 
 const main = async () => {
   try {
@@ -9,19 +9,14 @@ const main = async () => {
     //   transactionId: "0x8a84f5724c8a3bbd5996ee95b81d19d1350ddc6f3362e126a12df5bf41d6302c"
     // })
 
+    const userAddress = process.env.npm_config_useraddress || ''
+    if(!userAddress) {
+      console.log(`No userAddress set. Usage example: npm run portfolio --userAddress=0x4E430992Db6F3BdDbC6A50d1513845f087E9af4A.`)
+      process.exit(1)
+    }
+
     const swapXPools = await getInfo(userAddress)
-    console.log('swapX pools with rewards', swapXPools)
-    // console.log(`SwapX portfolio: user address=${
-    //   userAddress
-    // }, pool address=${
-    //   swapXInfo.poolAddress
-    // }, rewards earned: ${
-    //   swapXInfo.rewardsAmountFormatted
-    // }, rewards token: ${
-    //   swapXInfo.rewardTokenSymbol
-    // } (${
-    //   swapXInfo.rewardTokenAddress
-    // })`)
+    console.log(`User address: ${userAddress}, swapX pools with rewards:`, swapXPools)
   } catch (e) {
     console.error('failed to get portfolio data', e)
   }

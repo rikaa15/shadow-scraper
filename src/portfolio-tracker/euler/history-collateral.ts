57826eb2fa3fbbac7327b66771b44fda7e276a35
artemkolodko <temakolodko@gmail.com>
2025-05-04 17:01:01 +0200

    add euler historical collateral

--- src/portfolio-tracker/euler/history-collateral.ts
+++ src/portfolio-tracker/euler/history-collateral.ts
@@ -7,26 +7,17 @@ import EulerEVaultABI from '../../abi/EulerEVault.json'
 
 const provider = new ethers.JsonRpcProvider("https://rpc.soniclabs.com");
 const vaultAddress = '0x196F3C7443E940911EE2Bb88e019Fd71400349D9'
-// const vaultAddress = '0x3D9e5462A940684073EED7e4a13d19AE0Dcd13bc'
 const vault = new ethers.Contract(vaultAddress, EulerEVaultABI, provider);
-
-const SecondsPerYear = 365.2425 * 86400
-const NumberRay = 10 ** 27
-const CONFIG_SCALE = 10000
 
 export const getEulerCollateralHistory = async () => {
   const vaultName = await vault.name() as string
 
   const daysCount = 30
-  let minApr = new Decimal(1000)
-  let maxApr = new Decimal(0)
 
   console.log(`Euler vault ${vaultName}`)
   for(let i = 0; i < daysCount; i++) {
     const blockDate = moment().subtract(i + 1, "days").endOf('day')
     const blockTimestamp = blockDate.unix()
-    const blockDateStr = blockDate.format('YYYY-MM-DD HH:mm:ss')
-    // console.log(`Searching for the block for date=${blockDateStr}...`)
 
     const block = await getClosestBlockByTimestamp(
       blockTimestamp
@@ -38,9 +29,6 @@ export const getEulerCollateralHistory = async () => {
     }
 
     const blockTag = block.blockNumber
-
-    const interestRate = await vault.interestRate({ blockTag }) as bigint
-    const interestFee = await vault.interestFee({ blockTag }) as bigint
     const totalBorrows = await vault.totalBorrows({ blockTag }) as bigint
     const totalAssets = await vault.totalAssets({ blockTag }) as bigint
 
@@ -49,13 +37,23 @@ export const getEulerCollateralHistory = async () => {
       .mul(100)
 
     console.log(`[${
-      blockDate.format('YYYY-MM-DD')
-    }] total assets=${
-      totalAssets.toString()
-    }, total borrows=${
-      totalBorrows.toString()
-    }, utilization=${
-      utilization.toSD(4).toString()
+      blockDate.format('YYYY MMM DD')
+    }] total borrows=${
+      new Decimal(totalBorrows.toString())
+        .div(10 ** 6)
+        .div(10 ** 6)
+        .toSD(4)
+        .toString()
+    }M, total supply=${
+      new Decimal(totalAssets.toString())
+        .div(10 ** 6)
+        .div(10 ** 6)
+        .toSD(4)
+        .toString()
+    }M, utilization=${
+      utilization
+        .toSD(4)
+        .toString()
     }%`)
   }
 

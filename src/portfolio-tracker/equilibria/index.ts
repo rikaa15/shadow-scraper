aa04b31bf5748cb9a048d6f3118b01a58d9f9fcb
fegloff <fegloff@gmail.com>
2025-05-12 19:33:21 -0500

    add multiple pool inquiry to beefy and equilibria + update api call to use axios on beefy helper module

--- src/portfolio-tracker/equilibria/index.ts
+++ src/portfolio-tracker/equilibria/index.ts
@@ -7,24 +7,36 @@ import {
   findPoolId,
   getTokenInfo,
   getUserDepositInfo,
-} from './equilibriaHelper';
-import { portfolioItemFactory, roundToSignificantDigits, calculateDaysDifference } from "../helpers";
+} from "./equilibriaHelper";
+import {
+  portfolioItemFactory,
+  roundToSignificantDigits,
+  calculateDaysDifference,
+} from "../helpers";
 
 // Setup provider
 const provider = new ethers.JsonRpcProvider("https://rpc.soniclabs.com");
 
-// Contract addresses
-const MARKET_LP = '0x3F5EA53d1160177445B1898afbB16da111182418';
-
-const DEPOSIT_LINK = 'https://equilibria.fi/dashboard';
-
-const baseRewardPoolABI = [
-  'function getRewardTokens() view returns (address[])',
-  'function earned(address user, address rewardToken) view returns (uint256)',
-  'function balanceOf(address user) view returns (uint256)'
+const marketArray = [
+  {
+    name: "equilibria",
+    address: "0x3F5EA53d1160177445B1898afbB16da111182418",
+    type: "LP",
+  },
 ];
 
-async function getUnclaimedRewards(rewardPoolAddress: string, userAddress: string) {
+const DEPOSIT_LINK = "https://equilibria.fi/dashboard";
+
+const baseRewardPoolABI = [
+  "function getRewardTokens() view returns (address[])",
+  "function earned(address user, address rewardToken) view returns (uint256)",
+  "function balanceOf(address user) view returns (uint256)",
+];
+
+async function getUnclaimedRewards(
+  rewardPoolAddress: string,
+  userAddress: string
+) {
   // Create contract instance
   const rewardPoolContract = new ethers.Contract(
     rewardPoolAddress,
@@ -35,7 +47,7 @@ async function getUnclaimedRewards(rewardPoolAddress: string, userAddress: strin
   // Get reward tokens
   const rewardTokens = await rewardPoolContract.getRewardTokens();
 
-  console.log(`Found ${rewardTokens.length} reward tokens`);
+  // console.log(`Found ${rewardTokens.length} reward tokens`);
 
   // Get earned amounts for each token
   const rewards = await Promise.all(
@@ -43,14 +55,16 @@ async function getUnclaimedRewards(rewardPoolAddress: string, userAddress: strin
       const earned = await rewardPoolContract.earned(userAddress, token);
 
       const tokenInfo = await getTokenInfo(token);
-      const amount = new Decimal(earned.toString()).div(10 ** tokenInfo.decimals);
+      const amount = new Decimal(earned.toString()).div(
+        10 ** tokenInfo.decimals
+      );
 
       return {
         token,
         symbol: tokenInfo.symbol,
         decimals: tokenInfo.decimals,
         amount,
-        raw: earned
+        raw: earned,
       };
     })
   );
@@ -58,122 +72,147 @@ async function getUnclaimedRewards(rewardPoolAddress: string, userAddress: strin
   return rewards;
 }
 
-export async function getEquilibriaInfo(userAddress: string, marketAddress: string = MARKET_LP) {
-  try {
-    const portfolioItems: PortfolioItem[] = [];
+export async function getEquilibriaInfo(userAddress: string) {
+  const portfolioItems: PortfolioItem[] = [];
+  const marketPromises = marketArray.map(async (market) => {
+    try {
+      const marketAddress = market.address;
+      const { poolId, token, rewardPool } = await findPoolId(marketAddress);
+      console.log(
+        `For market ${marketAddress} found pool ID: ${poolId} with deposit token: ${token} and reward pool: ${rewardPool}`
+      );
 
-    console.log(`Finding pool ID for Market LP: ${marketAddress}`);
-    const { poolId, token, rewardPool } = await findPoolId(marketAddress);
-    console.log(`Found pool ID: ${poolId} with deposit token: ${token} and reward pool: ${rewardPool}`);
+      // Get token info
+      const depositTokenInfo = await getTokenInfo(token);
 
-    // Get token info
-    const depositTokenInfo = await getTokenInfo(token);
+      // Create reward pool contract
+      const rewardPoolContract = new ethers.Contract(
+        rewardPool,
+        baseRewardPoolABI,
+        provider
+      );
 
-    // Create reward pool contract
-    const rewardPoolContract = new ethers.Contract(
-      rewardPool,
-      baseRewardPoolABI,
-      provider
-    );
+      // Get user's balance in the reward pool
+      const userBalance = await rewardPoolContract.balanceOf(userAddress);
 
-    // Get user's balance in the reward pool
-    const userBalance = await rewardPoolContract.balanceOf(userAddress);
+      // If user has no balance, return empty array
+      if (userBalance === 0n) {
+        return null;
+      }
 
-    // If user has no balance, return empty array
-    if (userBalance === 0n) {
-      return portfolioItems;
-    }
+      const rewards = await getUnclaimedRewards(rewardPool, userAddress);
 
-    console.log(`\nGetting unclaimed rewards for user: ${userAddress}`);
-    const rewards = await getUnclaimedRewards(rewardPool, userAddress);
+      const walletDepositInfo = await getUserDepositInfo(
+        userAddress,
+        marketAddress
+      );
 
-    console.log('\nUnclaimed Rewards:');
-    rewards.forEach((reward) => {
-      console.log(`${reward.symbol}: ${reward.amount.toString()} (${reward.token})`);
-    });
+      const depositTimestamp = walletDepositInfo
+        ? walletDepositInfo.depositTimestamp
+        : new Date();
+      const depositAmount = walletDepositInfo
+        ? `${walletDepositInfo.totalDeposited}`
+        : "0";
+      const depositAmountUSD = walletDepositInfo
+        ? `${walletDepositInfo.totalDepositedUSD}`
+        : "0";
+      const depositDate = new Date(depositTimestamp);
+      const totalBlocks = walletDepositInfo ? walletDepositInfo.totalBlocks : 0;
+      // Process rewards
+      const mainReward = rewards[0]; // First reward token (usually PENDLE)
+      let secondaryReward = rewards[1]; // Second reward token (if exists)
 
-    const walletDepositInfo = await getUserDepositInfo(userAddress, marketAddress)
-
-    const depositTimestamp = walletDepositInfo ? walletDepositInfo.depositTimestamp : new Date()
-    const depositAmount = walletDepositInfo ? `${walletDepositInfo.totalDeposited}` : '0'
-    const depositAmountUSD = walletDepositInfo ? `${walletDepositInfo.totalDepositedUSD}` : '0'
-    const depositDate = new Date(depositTimestamp);
-    const totalBlocks = walletDepositInfo ? walletDepositInfo.totalBlocks : 0
-    // Process rewards
-    const mainReward = rewards[0]; // First reward token (usually PENDLE)
-    let secondaryReward = rewards[1]; // Second reward token (if exists)
-
-    // Calculate APR for main reward
-    const aprResult = await calculateTokenAPR(
-      mainReward.token,
-      mainReward.symbol,
-      mainReward.amount,
-      parseFloat(depositAmountUSD),
-      depositDate
-    );
-    // Calculate secondary APR if available
-    let secondaryAprResult = {
-      tokenPrice: 0,
-      apr: 0,
-      daysSinceDeposit: aprResult.daysSinceDeposit
-    };
-
-    if (secondaryReward) {
-
-      secondaryAprResult = await calculateTokenAPR(
-        secondaryReward.token,
-        secondaryReward.symbol,
-        secondaryReward.amount,
-        parseFloat(depositAmount),
+      // Calculate APR for main reward
+      const aprResult = await calculateTokenAPR(
+        mainReward.token,
+        mainReward.symbol,
+        mainReward.amount,
+        parseFloat(depositAmountUSD),
         depositDate
       );
+      // Calculate secondary APR if available
+      let secondaryAprResult = {
+        tokenPrice: 0,
+        apr: 0,
+        daysSinceDeposit: aprResult.daysSinceDeposit,
+      };
+
+      if (secondaryReward) {
+        secondaryAprResult = await calculateTokenAPR(
+          secondaryReward.token,
+          secondaryReward.symbol,
+          secondaryReward.amount,
+          parseFloat(depositAmount),
+          depositDate
+        );
+      }
+
+      // Calculate total APR
+      const totalApr =
+        aprResult.apr + (secondaryAprResult ? secondaryAprResult.apr : 0);
+
+      // Create portfolio item
+      const portfolioItem: PortfolioItem = {
+        ...portfolioItemFactory(),
+        type: market.type,
+        name: market.name,
+        address: marketAddress,
+        depositTime: moment(depositTimestamp).format("YY/MM/DD HH:MM:SS"),
+        depositAsset0: depositTokenInfo.symbol,
+        depositAsset1: "",
+        depositAmount0: roundToSignificantDigits(depositAmountUSD, 3),
+        depositAmount1: "",
+        depositValue0: roundToSignificantDigits(depositAmountUSD, 3),
+        depositValue1: "",
+        depositValue: roundToSignificantDigits(depositAmountUSD, 3),
+        rewardAsset0: mainReward.symbol,
+        rewardAsset1: secondaryReward ? secondaryReward.symbol : "",
+        rewardAmount0: roundToSignificantDigits(mainReward.amount.toString()),
+        rewardAmount1: secondaryReward
+          ? roundToSignificantDigits(secondaryReward.amount.toString())
+          : "",
+        rewardValue0: roundToSignificantDigits(
+          mainReward.amount.mul(aprResult.tokenPrice).toString()
+        ),
+        rewardValue1:
+          secondaryReward && secondaryAprResult
+            ? roundToSignificantDigits(
+                secondaryReward.amount
+                  .mul(secondaryAprResult.tokenPrice)
+                  .toString()
+              )
+            : "",
+        rewardValue: roundToSignificantDigits(
+          mainReward.amount
+            .mul(aprResult.tokenPrice)
+            .add(
+              secondaryReward && secondaryAprResult
+                ? secondaryReward.amount.mul(secondaryAprResult.tokenPrice)
+                : 0
+            )
+            .toString()
+        ),
+        totalDays: calculateDaysDifference(
+          new Date(depositTimestamp),
+          new Date(),
+          4
+        ),
+        totalBlocks: `${totalBlocks}`,
+        apr: roundToSignificantDigits(totalApr.toString()),
+        depositLink: DEPOSIT_LINK,
+      };
+
+      return portfolioItem;
+    } catch (error) {
+      console.error("Error getting Equilibria portfolio data:", error);
+      return null;
     }
+  });
 
-    // Calculate total APR
-    const totalApr = aprResult.apr + (secondaryAprResult ? secondaryAprResult.apr : 0);
+  const results = await Promise.all(marketPromises);
+  results.forEach((item) => {
+    if (item) portfolioItems.push(item);
+  });
 
-    // Create portfolio item
-    const portfolioItem: PortfolioItem = {
-      ...portfolioItemFactory(),
-      type: 'LP',
-      name: 'equilibria',
-      address: marketAddress,
-      depositTime: moment(depositTimestamp).format('YY/MM/DD HH:MM:SS'),
-      depositAsset0: depositTokenInfo.symbol,
-      depositAsset1: '',
-      depositAmount0: roundToSignificantDigits(depositAmountUSD, 3),
-      depositAmount1: '',
-      depositValue0: roundToSignificantDigits(depositAmountUSD, 3),
-      depositValue1: '',
-      depositValue: roundToSignificantDigits(depositAmountUSD, 3),
-      rewardAsset0: mainReward.symbol,
-      rewardAsset1: secondaryReward ? secondaryReward.symbol : '',
-      rewardAmount0: roundToSignificantDigits(mainReward.amount.toString()),
-      rewardAmount1: secondaryReward ? roundToSignificantDigits(secondaryReward.amount.toString()) : '',
-      rewardValue0: roundToSignificantDigits(
-        mainReward.amount.mul(aprResult.tokenPrice).toString()
-      ),
-      rewardValue1: secondaryReward && secondaryAprResult ?
-        roundToSignificantDigits(
-          secondaryReward.amount.mul(secondaryAprResult.tokenPrice).toString()
-        ) : '',
-      rewardValue: roundToSignificantDigits(
-        mainReward.amount.mul(aprResult.tokenPrice)
-          .add(secondaryReward && secondaryAprResult ?
-            secondaryReward.amount.mul(secondaryAprResult.tokenPrice) : 0)
-          .toString()
-      ),
-      totalDays: calculateDaysDifference(new Date(depositTimestamp), new Date(), 4),
-      totalBlocks: `${totalBlocks}`,
-      apr: roundToSignificantDigits(totalApr.toString()),
-      depositLink: DEPOSIT_LINK
-    };
-
-    portfolioItems.push(portfolioItem);
-    return portfolioItems;
-
-  } catch (error) {
-    console.error('Error getting Equilibria portfolio data:', error);
-    return [];
-  }
+  return portfolioItems;
 }

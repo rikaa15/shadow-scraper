4654f6c0a9c9cd890c67f36826d0265047167959
Rika <15hatoyar@gmail.com>
2025-05-20 12:11:47 -0700

    Initialize subgraph

---
+++ src/subgraph/silo-finance/silo-router/src/silo-router-v-2.ts
@@ -0,0 +1,70 @@
+import {
+  OwnershipTransferStarted as OwnershipTransferStartedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Paused as PausedEvent,
+  Unpaused as UnpausedEvent
+} from "../generated/SiloRouterV2/SiloRouterV2"
+import {
+  OwnershipTransferStarted,
+  OwnershipTransferred,
+  Paused,
+  Unpaused
+} from "../generated/schema"
+
+export function handleOwnershipTransferStarted(
+  event: OwnershipTransferStartedEvent
+): void {
+  let entity = new OwnershipTransferStarted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePaused(event: PausedEvent): void {
+  let entity = new Paused(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnpaused(event: UnpausedEvent): void {
+  let entity = new Unpaused(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ src/subgraph/silo-finance/silo-router/tests/silo-router-v-2.test.ts
@@ -0,0 +1,60 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address } from "@graphprotocol/graph-ts"
+import { OwnershipTransferStarted } from "../generated/schema"
+import { OwnershipTransferStarted as OwnershipTransferStartedEvent } from "../generated/SiloRouterV2/SiloRouterV2"
+import { handleOwnershipTransferStarted } from "../src/silo-router-v-2"
+import { createOwnershipTransferStartedEvent } from "./silo-router-v-2-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let previousOwner = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newOwner = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newOwnershipTransferStartedEvent = createOwnershipTransferStartedEvent(
+      previousOwner,
+      newOwner
+    )
+    handleOwnershipTransferStarted(newOwnershipTransferStartedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("OwnershipTransferStarted created and stored", () => {
+    assert.entityCount("OwnershipTransferStarted", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "OwnershipTransferStarted",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "previousOwner",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "OwnershipTransferStarted",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "newOwner",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
